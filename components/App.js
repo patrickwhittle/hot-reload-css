2e7071b8f43849a727a65ada322efd33e2eedf61
Patrick Whittle <patrick.whittle@bookassist.com>
2016-12-15 08:44:23 -0800

    initial commit

---
+++ components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../styles/main.css!'
+
+class App extends React.Component {
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+
+}
+
+export default App
+
